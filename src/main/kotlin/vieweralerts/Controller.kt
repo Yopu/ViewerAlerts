d23314d85fce0de28838c8eb92de063e29bd4be2
Yopu <yopu42@gmail.com>
2015-12-15 08:41:58 -0500

    Converted to MVC via FXML

---
+++ src/main/kotlin/vieweralerts/Controller.kt
@@ -0,0 +1,98 @@
+package vieweralerts
+
+import javafx.animation.FillTransition
+import javafx.application.Platform
+import javafx.collections.FXCollections.observableArrayList
+import javafx.collections.ListChangeListener
+import javafx.collections.ObservableList
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
+import javafx.fxml.Initializable
+import javafx.scene.control.Button
+import javafx.scene.control.Label
+import javafx.scene.control.ListView
+import javafx.scene.paint.Color
+import javafx.scene.shape.Rectangle
+import javafx.util.Duration
+import java.net.URL
+import java.util.*
+
+class Controller : Initializable {
+
+    val allUsersList = observableArrayList<String>()
+    val newUsersList = observableArrayList<String>()
+
+    @FXML lateinit var allUsersListView: ListView<String>
+    @FXML lateinit var allUsersCountLabel: Label
+
+    @FXML lateinit var newUsersListView: ListView<String>
+    @FXML lateinit var newUsersCountLabel: Label
+
+
+    @FXML lateinit var clearButton: Button
+    @FXML lateinit var alertRectangle: Rectangle
+    var transition: FillTransition? = null
+
+    override fun initialize(location: URL?, resources: ResourceBundle?) {
+        allUsersListView.items = allUsersList
+        newUsersListView.items = newUsersList
+
+        allUsersList.addSizeLabel(allUsersCountLabel)
+        newUsersList.addSizeLabel(newUsersCountLabel)
+
+        clearButton.prefWidthProperty().bind(newUsersListView.widthProperty())
+        alertRectangle.heightProperty().bind(clearButton.heightProperty())
+        alertRectangle.widthProperty().bind(clearButton.widthProperty())
+
+
+        allUsersList.addListener(ListChangeListener {
+            while (it.next()) {
+                if (it.wasAdded())
+                    newUsersList.addAll(it.addedSubList)
+                if (it.wasRemoved())
+                    newUsersList.removeAll(it.removed)
+            }
+        })
+
+        newUsersList.addListener(ListChangeListener {
+            var added = false
+            while (it.next()) {
+                if (it.wasAdded())
+                    added = true
+            }
+            if (added)
+                flashAlert()
+        })
+    }
+
+    fun closeMenuPressed(event: ActionEvent) {
+        Platform.exit()
+    }
+
+    fun clearButtonPressed(event: ActionEvent) {
+        newUsersList.clear()
+        stopFlashing()
+    }
+
+    fun flashAlert() {
+        alertRectangle.isVisible = true
+
+        transition = FillTransition(Duration.seconds(0.5), alertRectangle, Color.TRANSPARENT, Color.RED).apply {
+            cycleCount = FillTransition.INDEFINITE
+            isAutoReverse = true
+            play()
+        }
+    }
+
+    fun stopFlashing() {
+        alertRectangle.isVisible = false
+        transition?.stop()
+    }
+
+    fun <T> ObservableList<T>.addSizeLabel(label: Label) {
+        label.text = size.toString()
+        addListener(ListChangeListener {
+            label.text = size.toString()
+        })
+    }
+}
--- src/main/kotlin/vieweralerts/main.kt
+++ src/main/kotlin/vieweralerts/main.kt
@@ -3,127 +3,48 @@ package vieweralerts
 import com.beust.klaxon.JsonArray
 import com.beust.klaxon.JsonObject
 import com.beust.klaxon.Parser
-import javafx.animation.FillTransition
-import javafx.animation.FillTransition.INDEFINITE
 import javafx.application.Application
 import javafx.application.Application.launch
-import javafx.application.Platform.runLater
-import javafx.collections.FXCollections
-import javafx.collections.ListChangeListener
+import javafx.application.Platform
 import javafx.collections.ObservableList
+import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
-import javafx.scene.control.Button
-import javafx.scene.control.ListView
-import javafx.scene.layout.HBox
-import javafx.scene.layout.StackPane
 import javafx.scene.layout.VBox
-import javafx.scene.paint.Color
-import javafx.scene.shape.Rectangle
 import javafx.stage.Stage
-import javafx.util.Duration
 import java.net.URL
 import kotlin.concurrent.thread
+
+val CHANNEL = "sheevergaming"
 
 fun main(args: Array<String>) = launch(DisplayApplication::class.java, *args)
 
 class DisplayApplication : Application() {
-
-    private lateinit var rectangle: Rectangle
-    private var transition: FillTransition? = null
-
     override fun start(primaryStage: Stage) {
-        val newUsersList = FXCollections.observableArrayList<String>()
-        newUsersList.addListener(ListChangeListener {
-            var added = false
-            while (it.next()) {
-                if (it.wasAdded())
-                    added = true
-            }
-            if (added)
-                flashAlert()
-        })
+        val fxmlLoader = FXMLLoader(ClassLoader.getSystemResource("main.fxml"))
+        val vBox = fxmlLoader.load<VBox>()
 
+        val controller = fxmlLoader.getController<Controller>()
+        startLooperThread(CHANNEL, controller.allUsersList, 10000)
 
-        val allUsersList = FXCollections.observableArrayList<String>()
-        allUsersList.addListener(ListChangeListener {
-            while (it.next()) {
-                if (it.wasAdded())
-                    newUsersList.addAll(it.addedSubList)
-            }
-        })
-
-
-        startLooperThread(allUsersList, 10000)
-
-
-        val allUsersListView = ListView<String>(allUsersList)
-        allUsersListView.isFocusTraversable = false
-
-
-        val newUsersListView = ListView<String>(newUsersList)
-        newUsersListView.isFocusTraversable = false
-
-
-        val clearButton = Button("Clear")
-        clearButton.prefWidthProperty().bind(newUsersListView.widthProperty())
-        clearButton.setOnAction {
-            newUsersList.clear()
-            stopFlashing()
-        }
-
-
-        rectangle = Rectangle()
-        rectangle.isMouseTransparent = true
-        rectangle.isVisible = false
-        rectangle.heightProperty().bind(clearButton.heightProperty())
-        rectangle.widthProperty().bind(newUsersListView.widthProperty())
-
-
-        val buttonStackPane = StackPane(clearButton, rectangle)
-
-
-        val vBox = VBox(newUsersListView, buttonStackPane)
-        vBox.maxHeightProperty().bind(allUsersListView.heightProperty())
-        val rootHBox = HBox(allUsersListView, vBox)
-
-        primaryStage.scene = Scene(rootHBox)
-        primaryStage.isResizable = false
+        primaryStage.scene = Scene(vBox)
         primaryStage.show()
-    }
-
-
-    fun flashAlert() {
-        rectangle.isVisible = true
-
-        transition = FillTransition(Duration.seconds(0.5), rectangle, Color.TRANSPARENT, Color.RED).apply {
-            cycleCount = INDEFINITE
-            isAutoReverse = true
-            play()
-        }
-    }
-
-    fun stopFlashing() {
-        rectangle.isVisible = false
-        transition?.stop()
     }
 }
 
-fun startLooperThread(observableList: ObservableList<String>, sleepDuration: Long): Thread {
+fun startLooperThread(channel: String, observableList: ObservableList<String>, sleepDuration: Long): Thread {
     val thread = thread(isDaemon = true) {
         while (true) {
-            val newUsers = downloadUsers().filterNot { observableList.contains(it) }.toList()
-            runLater { observableList.addAll(newUsers) }
+            val newUsers = downloadUsers(channel).filterNot { observableList.contains(it) }.toList()
+            Platform.runLater { observableList.addAll(newUsers) }
             Thread.sleep(sleepDuration)
         }
     }
     return thread
 }
 
-fun downloadUsers(): List<String> {
-    val url = URL("http://tmi.twitch.tv/group/user/mrssteelix/chatters")
+fun downloadUsers(channel: String): List<String> {
+    val url = URL("http://tmi.twitch.tv/group/user/$channel/chatters")
     val parsed = Parser().parse(url.openStream()) as JsonObject
     val chatters = parsed["chatters"] as JsonObject
     return chatters.values.flatMap { it as JsonArray<String> }
 }
-
-
